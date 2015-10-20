eeecaf5635b03e009a8eb172112b557917dfd9e8
lizthebiz <lizdalay@gmail.com>
2015-10-20 11:49:31 -0700

    Initial Commit

---
+++ mailbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mailbox
+//
+//  Created by Liz Dalay on 10/20/15.
+//  Copyright © 2015 Liz Dalay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

affd29d22fa69aaf950408f47671f588416416a6
lizthebiz <lizdalay@gmail.com>
2015-10-24 23:56:03 -0700

    Swiping left works; swiping right almost wrapped up

--- mailbox/MailboxViewController.swift
+++ mailbox/MailboxViewController.swift
@@ -31,6 +31,12 @@ class MailboxViewController: UIViewController, UIGestureRecognizerDelegate {
     @IBOutlet weak var archiveIconImageView: UIImageView!
     
     var messageInitialFrame: CGPoint!
+    var feedInitialFrame: CGPoint!
+    
+    var laterIconInitialFrame: CGPoint!
+    var listIconInitialFrame: CGPoint!
+    var deleteIconInitialFrame: CGPoint!
+    var archiveIconInitialFrame: CGPoint!
     
     func UIColorFromRGB(rgbValue: UInt) -> UIColor {
         return UIColor(
@@ -75,13 +81,21 @@ class MailboxViewController: UIViewController, UIGestureRecognizerDelegate {
         // Absolute (x,y) coordinates in parent view
         let point = sender.locationInView(view)
         // Relative change in (x,y) coordinates from where gesture began.
-        let messageTranslation = sender.translationInView(view)
+        let translation = sender.translationInView(view)
+        let deleteIconTranslation = sender.translationInView(view)
         let velocity = sender.velocityInView(view)
         
         if sender.state == UIGestureRecognizerState.Began {
             print("Gesture began at: \(point)")
             
             messageInitialFrame = messageImageView.frame.origin
+            
+            feedInitialFrame = feedImageView.frame.origin
+            
+            laterIconInitialFrame = laterIconImageView.frame.origin
+            listIconInitialFrame = listIconImageView.frame.origin
+            deleteIconInitialFrame = deleteIconImageView.frame.origin
+            archiveIconInitialFrame = archiveIconImageView.frame.origin
             
             laterIconImageView.alpha = 0
             listIconImageView.alpha = 0
@@ -92,6 +106,8 @@ class MailboxViewController: UIViewController, UIGestureRecognizerDelegate {
             print("Gesture changed at: \(point)")
             
             if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x <= 60 {
+                messageView.backgroundColor = UIColorFromRGB(0xE5E6E9)
+                
                 UIView.animateWithDuration(0.8, animations: {
                 self.archiveIconImageView.alpha = 1
                 })
@@ -99,41 +115,113 @@ class MailboxViewController: UIViewController, UIGestureRecognizerDelegate {
             else if messageImageView.frame.origin.x >= 60 && messageImageView.frame.origin.x < 260 {
                 messageView.backgroundColor = UIColorFromRGB(0x1ED200)
                 self.archiveIconImageView.alpha = 1
+                self.deleteIconImageView.alpha = 0
+                
+                self.archiveIconImageView.frame.origin.x = CGFloat(self.archiveIconInitialFrame.x + translation.x - 62)
             }
             else if messageImageView.frame.origin.x >= 260 {
                 messageView.backgroundColor = UIColorFromRGB(0xFF0000)
+                self.archiveIconImageView.alpha = 0
+                self.deleteIconImageView.alpha = 1
+                
+                self.deleteIconImageView.frame.origin.x = CGFloat(self.deleteIconInitialFrame.x + translation.x - 280)
             }
             else if messageImageView.frame.origin.x < 0 && messageImageView.frame.origin.x >= -60 {
+                messageView.backgroundColor = UIColorFromRGB(0xE5E6E9)
+                
                 UIView.animateWithDuration(0.8, animations: {
                     self.laterIconImageView.alpha = 1
                 })
             }
-            else if messageImageView.frame.origin.x <= -60 && messageImageView.frame.origin.x > -260 {
-            messageView.backgroundColor = UIColorFromRGB(0xFECA16)
-                    
-                UIView.animateWithDuration(0.2, animations: {
+            else if messageImageView.frame.origin.x < -60 && messageImageView.frame.origin.x > -260 {
+                messageView.backgroundColor = UIColorFromRGB(0xFECA16)
                 self.laterIconImageView.alpha = 1
-                })
+                self.listIconImageView.alpha = 0
+                
+                self.laterIconImageView.frame.origin.x = CGFloat(self.laterIconInitialFrame.x + translation.x + 62)
             }
             else if messageImageView.frame.origin.x < -260 {
                 messageView.backgroundColor = UIColorFromRGB(0xDBAA82)
                 self.laterIconImageView.alpha = 0
                 self.listIconImageView.alpha = 1
-                }
+                
+                self.listIconImageView.frame.origin.x = CGFloat(self.listIconInitialFrame.x + translation.x + 270)
+            }
             
-            messageImageView.frame.origin.x = CGFloat(messageInitialFrame.x + messageTranslation.x)
+            messageImageView.frame.origin.x = CGFloat(messageInitialFrame.x + translation.x)
             print("\(messageImageView.frame.origin.x)")
             
         } else if sender.state == UIGestureRecognizerState.Ended {
             print("Gesture ended at: \(point)")
             
-            UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 2, initialSpringVelocity: 0, options: [], animations: { () -> Void in
+            if messageImageView.frame.origin.x > 0 && messageImageView.frame.origin.x <= 60 {
+                UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 2, initialSpringVelocity: 0, options: [], animations: { () -> Void in
+                    
+                    self.messageView.backgroundColor = self.UIColorFromRGB(0xE5E6E9)
+                    self.laterIconImageView.alpha = 1
+                    self.messageImageView.frame.origin.x = CGFloat(self.messageInitialFrame.x)
+                    
+                    }, completion: nil)
+                }
+            
+            else if messageImageView.frame.origin.x >= 60 && messageImageView.frame.origin.x < 260 {
+                self.archiveIconImageView.alpha = 0
+                self.deleteIconImageView.alpha = 0
                 
-                self.messageImageView.frame.origin.x = CGFloat(self.messageInitialFrame.x)
-                self.messageView.backgroundColor = self.UIColorFromRGB(0xE5E6E9)
+                UIView.animateWithDuration(0.3) { () -> Void in
+                self.messageImageView.frame.origin.x = CGFloat(self.messageInitialFrame.x + 400)
+                self.feedImageView.frame.origin.y = CGFloat(self.feedInitialFrame.y - 86)
+                }
+                UIView.animateWithDuration(0.1) { () -> Void in self.messageView.alpha = 0
+                }
+            }
+            else if messageImageView.frame.origin.x >= 260 {
                 
-                }, completion: nil)
-           }
+                messageView.backgroundColor = UIColorFromRGB(0xFF0000)
+                self.archiveIconImageView.alpha = 0
+                self.deleteIconImageView.alpha = 1
+                
+                UIView.animateWithDuration(0.3) { () -> Void in
+                    self.messageImageView.frame.origin.x = CGFloat(self.messageInitialFrame.x + 400)
+                    self.feedImageView.frame.origin.y = CGFloat(self.feedInitialFrame.y - 86)
+                }
+                UIView.animateWithDuration(0.1) { () -> Void in self.messageView.alpha = 0
+                }
+            }
+            else if messageImageView.frame.origin.x < 0 && messageImageView.frame.origin.x > -60 {
+                UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 2, initialSpringVelocity: 0, options: [], animations: { () -> Void in
+                    
+                    self.messageView.backgroundColor = self.UIColorFromRGB(0xE5E6E9)
+                    self.laterIconImageView.alpha = 1
+                    self.messageImageView.frame.origin.x = CGFloat(self.messageInitialFrame.x)
+                    
+                    }, completion: nil)
+            }
+            else if messageImageView.frame.origin.x <= -60 && messageImageView.frame.origin.x > -260 {
+                messageView.backgroundColor = UIColorFromRGB(0xFECA16)
+                self.laterIconImageView.alpha = 0
+                self.listIconImageView.alpha = 0
+                
+                UIView.animateWithDuration(0.3) { () -> Void in
+                    self.messageImageView.frame.origin.x = CGFloat(self.messageInitialFrame.x - 100)
+                }
+                UIView.animateWithDuration(0.1) { () -> Void in self.messageImageView.alpha = 0
+                }
+            }
+            else if messageImageView.frame.origin.x <= -260 {
+                
+                messageView.backgroundColor = UIColorFromRGB(0xDBAA82)
+                self.laterIconImageView.alpha = 0
+                self.listIconImageView.alpha = 1
+                
+                UIView.animateWithDuration(0.3) { () -> Void in
+                    self.messageImageView.frame.origin.x = CGFloat(self.messageInitialFrame.x + 400)
+                }
+                UIView.animateWithDuration(0.1) { () -> Void in self.messageImageView.alpha = 0
+                }
+            }
+            
+          }
     }
 
 }
